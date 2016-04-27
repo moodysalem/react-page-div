f5c23a45f4c23f4d439cb71a1896e8a85f87b302
Moody Salem <moody@fastmodeltechnologies.com>
2016-04-27 16:28:53 -0500

    fix tests

--- test.js
+++ test.js
@@ -1,30 +1,31 @@
-const React = require('react'),
-  DOM = React.DOM,
-  ReactDOM = require('react-dom'),
-  PageDiv = require('./index'),
-  PageDivFactory = React.createFactory(PageDiv),
-  assert = require('assert'),
+import React, { DOM, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { ReactPageDivFactory } from './index';
+
+const assert = require('assert'),
   jsdom = require('jsdom').jsdom;
 
-window = jsdom('<html><body><div id="test-container"></div></body></html>').defaultView;
+global.window = jsdom('<html><body><div id="test-container"></div></body></html>').defaultView;
+global.document = window.document;
 
-const testContainer = window.document.getElementById('test-container');
+const testContainer = document.getElementById('test-container');
 
 describe('exports', function () {
   it('should be a function', function () {
-    assert(typeof PageDiv === 'function');
-    assert(typeof PageDivFactory === 'function');
+    assert(typeof ReactPageDivFactory === 'function');
   });
 });
 
 describe('height calculation', function () {
   it('should calculate some increment of page height', function (done) {
-    ReactDOM.render(PageDivFactory({
+    ReactDOM.render(ReactPageDivFactory({
       height: 11,
       heightUnit: 'in',
       width: 8.5,
       widthUnit: 'in'
     }, DOM.div({ height: 400 }, 'Hello World')), testContainer);
+
+    done();
 
     //console.log(testContainer.innerHTML);
   });
